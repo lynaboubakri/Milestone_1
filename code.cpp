39b66b080ea15105f6a446c854e52af6bb635a24
Mohammed Adel Ali <mohamedtwin1995@gmail.com>
2025-12-23 21:45:03 +0800

    Update code.cpp

--- code.cpp
+++ code.cpp
@@ -9,8 +9,26 @@ const int MAX_COLS = 10;
 void createSheetName(string &sheetName);
 void setupColumns(string colNames[], string colTypes[], int &numCols);
 void insertRow (string table[][MAX_COLS], int &rowCount, string colTypes[], int numCols);
-void displayCSV (string table[][MAX_COLS], int rowCount, string colNames[], int numCols);
-
+void displayCSV (string table[][MAX_COLS], int rowCount, string colNames[], int numCols); 
+// is validint function
+bool isValidInt(const string& str) {
+if (str.empty()) {
+return false;
+}
+int startIndex = 0;
+if (str.[0] == '-') { // if first character is a minus sign
+if (str.length() == 1) { // if its only a minus sign (no digits)
+return false; // thats not valid
+}
+startIndex = 1;
+}
+for (int i = startIndex; i < str.length(); i++) {
+if (!isdigit(str[i])) { // if this character is not a digit (0-9)
+return false; // Invalid - return false
+}
+}
+return ture; // all characters are digits - its valid
+}
 int main() {
     string sheetName;
     string columnNames[MAX_COLS];
@@ -79,3 +97,37 @@ void displayCSV ( string table [] [MAX_COLS] , int rowCount , string colNames []
     } 
 
 } 
+// insertRow implementation
+void insertRow (string table[][MAX_COLS], int &rowCount, string colTypes[], int cumCols)
+cout << "\n___________________________________________________\n";
+cout << "              Insert New Attendance Row\n";
+cout << "___________________________________________________\n";
+string input;
+for (int col =0; col < numCols; col++) { // loop through each column
+if (colTpes[col] == "INT") { // If this column is an integer
+while (true) { // keep looping until we get a valid input
+cout << "Enter column " << (col + 1) << " value: ";
+cin >> input; // Gets input string
+if (isValidInt(input)) { // Check if its a valid integer using function
+table[rowCount][col] = input; // store it into the table at current row
+break; // Exit the loop - we got a valid input
+} else {
+cout << "Error: Invaldi INT value. Please enter a number.\n"; // show error
+}
+}
+}
+else if (colTpes[col] == "TEXT") { // if this column needs text
+cout << "Enter column " << (col + 1) << " value: ";
+cin.ignore ();
+getline(cin, input); // reads the entire line including spaces
+while (input.empty()) {
+cout << "Error: Value cannot be empty.\n";
+cout << Enter column " << (col + 1) << " value: ";
+getline(cin. input); // Ask again
+}
+table[rowCount][col] = input; // stores the text in the table
+}
+}
+rowCount++; // Increase row count because we just a row
+cout << "Row inserted successfully.\n";
+}
